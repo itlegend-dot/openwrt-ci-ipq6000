bd8342020026e05d7c6ed1113a01ba1a8c37d3db
Jinpeng Peng <35778015+ixmu@users.noreply.github.com>
2026-06-22 20:56:15 +0800

    Update libwrt.sh

--- scripts/libwrt.sh
+++ scripts/libwrt.sh
@@ -1,3 +1,7 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+
+
+#保留自定义文件
+echo "/etc/owut.d/" >> package/base-files/files/etc/sysupgrade.conf
